7c7d14d5cb0a4dc683b686a60430789218542523
Rehxco <salome.brchrd@gmail.com>
2026-02-19 18:59:37 +0100

    debut contact

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,12 +6,13 @@
 /*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 16:11:50 by sbrochar          #+#    #+#             */
-/*   Updated: 2026/02/18 17:32:12 by sbrochar         ###   ########.fr       */
+/*   Updated: 2026/02/19 18:15:09 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contacts.hpp"
 #include "PhoneBook.hpp"
+
 
 int	main(int ac, char **av)
 {
@@ -27,28 +28,15 @@ int	main(int ac, char **av)
 		{
 			if (command == "ADD")
 			{
-				std::cout << "First Name: ";
-				std::cin >> buffer;
-				contact.set_first_name(buffer);
-				std::cout << "Last Name: ";
-				std::cin >> buffer;
-				contact.set_last_name(buffer);
-				std::cout << "Nickname: ";
-				std::cin >> buffer;
-				contact.set_nickname(buffer);
-				std::cout << "Phone Number: ";
-				std::cin >> buffer;
-				contact.set_phone_number(buffer);
-				std::cout << "Darkest Secret: ";
-				std::cin >> buffer;
-				contact.set_darkest_secret(buffer);
+				phone.contact_creation(contact);
+				
 			}
 			else if (command == "SEARCH")
 			{
 				std::cout << "Index * First Name * Last Name  * Nickname  " << std::endl;
 				std::cout << "      *            *            *           " << std::endl;
 				std::cout << "Index *            *            *           " << std::endl;
-				std::cout << "      *   *            *           " << std::endl;
+				std::cout << contact.get_first_name() << std::endl;
 			}
 			else if (command == "END")
 				return (0);
--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,18 +6,48 @@
 /*   By: sbrochar <sbrochar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 13:22:21 by sbrochar          #+#    #+#             */
-/*   Updated: 2026/02/18 14:01:11 by sbrochar         ###   ########.fr       */
+/*   Updated: 2026/02/19 18:54:26 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Contacts.hpp"
-#include <string>
 #include <iostream>
+#include <string>
 
 class PhoneBook
 {
   public:
-	int *contacts;
+	PhoneBook()
+	{
+		index = 0;
+	}
+	void add_contact()
+	{
+		std::string buffer;
+
+		std::cout << "First Name: ";
+		std::cin >> buffer;
+		tab[index].set_first_name(buffer);
+		std::cout << "Last Name: ";
+		std::cin >> buffer;
+		tab[index].set_last_name(buffer);
+		std::cout << "Nickname: ";
+		std::cin >> buffer;
+		tab[index].set_nickname(buffer);
+		std::cout << "Phone Number: ";
+		std::cin >> buffer;
+		tab[index].set_phone_number(buffer);
+		std::cout << "Darkest Secret: ";
+		std::cin >> buffer;
+		tab[index].set_darkest_secret(buffer);
+		index++;
+		if (index > 7)
+			index = 0;
+	}
+
+  private:
+	Contacts tab[8];
+	int index;
 };
